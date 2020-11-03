e51d73f7712d3185ad238741c87ab767fbf39f8a
mggoro <marting@epidataconsulting.com>
2020-11-03 15:40:56 -0300

    modificacion de prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          aws - mauricio !!!!!!!
+          aws - mauricio - modificacion de prueba !!!!!!!
         </p>
 
       </header>
