56bbd6dd9f70657506db24b11169f5009360a255
mggoro <marting@epidataconsulting.com>
2020-11-03 15:53:27 -0300

    modificado

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          aws - mauricio - otro branch Amplify!!!!!!!
+          aws - mauricio - otro branch Amplify - modificado!!!!!!!
         </p>
 
       </header>
